b6c14d64748a5e87a4c8aafc1473968973cd5071
tacolegs2004 <tylerchall16@gmail.com>
2022-12-08 16:45:46 -0600

    even more code

--- classes.py
+++ classes.py
@@ -1,22 +1,39 @@
 # classes
 
-class Dog:
+class Pet:
+    def walk(self):
+        print('walking...')
+
+
+class Dog(Pet):
     def __init__(self, name, age):
         self.name = name
         self.age = age
     def bark(self):
         print("woof")
+        
+class Cat(Pet):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def meow(self):
+        print("meow")
 
 
 
 roger = Dog("roger", 15)
-
+perry = Cat("perry", 4)
 print(type(roger))
-
+print(type(perry))
 print(roger.name)
 print(roger.age)
-print(roger.bark())
-
+roger.bark()
+roger.walk()
+print(perry.name)
+print(perry.age)
+perry.meow()
+perry.walk()
 
 
 
